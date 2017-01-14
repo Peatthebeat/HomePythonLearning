50effa3f22637d1c026ef1094dbdfe07ed4b5630
Pierre-Luc Morais <pierrelucmorais@gmail.com>
2017-01-14 14:03:26 -0500

    Create twilio_example.py

---
+++ Examples/twilio_example.py
@@ -0,0 +1,12 @@
+#!python3
+# twilio_example.py - Program that tests SMS functionality in Python using Twilio module
+#
+
+from twilio.rest import TwilioRestClient
+accountSID = 'AC92f6ed5882d5f9a622b3891c61beb04c'
+authToken = '992861d7bb1346acf7d65fdce12892c0'
+twilioCli = TwilioRestClient(accountSID, authToken)
+myTwilioNumber = '+18192011324'
+myCellphone = input('enter the phone number you\'d like to send a text message: \n')
+messagebody = input('please enter the message you want to send: \n')
+message = twilioCli.messages.create(body=messagebody, from_=myTwilioNumber, to=myCellphone)
